c4db53504b5d419b4329e0ec58bd4ef62494ef5f
Your Name <you@example.com>
2026-08-18 10:05:36 +0500

    Updated submodule content

--- Projects/Project Management – Mobile App UI/component/nav/nav.js
+++ Projects/Project Management – Mobile App UI/component/nav/nav.js
@@ -10,7 +10,13 @@ export class Nav extends HTMLElement {
         try {
             const css = await loadCSS("component/nav/nav.css");
             const html = await loadHTML("component/nav/nav.html");
-            this.shadowRoot.innerHTML = `<style>${css}</style>${html}`;
+            this.shadowRoot.innerHTML = `
+                <style>
+                    @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css");
+                </style>
+                <style>${css}</style>
+                ${html}
+            `;
         } catch (error) {
             console.error("Error loading nav component:", error);
         }
